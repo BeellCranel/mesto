edc30fded167460c6f5abc491b81c01f455af724
Dergach Roman <dergachroma@gmail.com>
2021-11-18 15:37:46 +0300

    feat: написать скрипты для попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,9 @@
-const editButton = document.querySelector('.profile__edit-button');
-
-const buttonOne = document.querySelector('.grid-card__button_place_one');
-const buttonTwo = document.querySelector('.grid-card__button_place_two');
-const buttonThree = document.querySelector('.grid-card__button_place_three');
-const buttonFour = document.querySelector('.grid-card__button_place_four');
-const buttonFive = document.querySelector('.grid-card__button_place_five');
-const buttonSix = document.querySelector('.grid-card__button_place_six');
-const popup = document.querySelector('.popup');
-
 // popup
 
+
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupClose = popup.querySelector('.popup__close-button');
 
 function openPopup() {
   popup.classList.add('popup__opened');
@@ -17,7 +11,42 @@ function openPopup() {
 
 editButton.addEventListener('click', openPopup);
 
+function closePopup() {
+  popup.classList.remove('popup__opened');
+}
+
+popupClose.addEventListener('click', closePopup);
+
+//submit
+
+
+const form = popup.querySelector('.edit-form');
+const inputUserName = popup.querySelector('.form__user-name');
+const inputDescription = popup.querySelector('.form__description');
+const profileUserName = document.querySelector('.profile__user-name');
+const profileDescriptoin = document.querySelector('.profile__user-description');
+const inputSubmit = popup.querySelector('.edit-form__submit');
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  profileUserName.textContent = inputUserName.value;
+  inputDescription.textContent = profileDescriptoin.value;
+}
+
+form.addEventListener('submit', submitForm);
+
+inputSubmit.addEventListener('click', closePopup);
+
 // likeButton
+
+
+const buttonOne = document.querySelector('.grid-card__button_place_one');
+const buttonTwo = document.querySelector('.grid-card__button_place_two');
+const buttonThree = document.querySelector('.grid-card__button_place_three');
+const buttonFour = document.querySelector('.grid-card__button_place_four');
+const buttonFive = document.querySelector('.grid-card__button_place_five');
+const buttonSix = document.querySelector('.grid-card__button_place_six');
 
 
 function likeOne() {
