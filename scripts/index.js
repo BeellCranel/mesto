c8cf08952241cd428ece7d85ee6025d985908ffe
Dergach Roman <dergachroma@gmail.com>
2021-11-25 19:23:03 +0300

    style: изменил скассы и добавил новые, что ты сделать работу функций боле гибкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
-const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('.popup_edit');
 const popupClose = popup.querySelector('.popup__close-button');
-const form = popup.querySelector('.form');
+const editForm = popup.querySelector('.form_edit');
 const inputUserName = popup.querySelector('.form__input_type_user-name');
 const inputDescription = popup.querySelector('.form__input_type_user-description');
 const profileUserName = document.querySelector('.profile__user-name');
 const profileDescriptoin = document.querySelector('.profile__user-description');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openEditPopup() {
+  editPopup.classList.add('popup_opened');
   inputUserName.value = profileUserName.textContent;
   inputDescription.value = profileDescriptoin.textContent;
 }
@@ -17,13 +18,13 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function submitForm(evt) {
+function submitEditForm(evt) {
   evt.preventDefault();
   profileUserName.textContent = inputUserName.value;
   profileDescriptoin.textContent = inputDescription.value;
   closePopup();
 }
 
-editButton.addEventListener('click', openPopup);
+editButton.addEventListener('click', openEditPopup);
 popupClose.addEventListener('click', closePopup);
-form.addEventListener('submit', submitForm);
+editForm.addEventListener('submit', submitEditForm);
