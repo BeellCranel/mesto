7c5155ec8f70477817284b33a5188c2e05218914
Dergach Roman <dergachroma@gmail.com>
2022-01-22 18:04:48 +0300

    refactor: Создал класс Попап

---
+++ scripts/Popup.js
@@ -0,0 +1,40 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = popupSelector;
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    this.setEventListeners();
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    this.removeEventListeners();
+  }
+
+  _handleEscClose = (evt) => {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  _handleButtonClose = (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      this.close();
+    }
+    if (evt.target.classList.contains('popup__close-button')) {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    document.addEventListener('keydown', this._handleEscClose);
+    this._popup.addEventListener('click', this._handleButtonClose);
+  }
+
+  removeEventListeners() {
+    document.removeEventListener('keydown', this._handleEscClose);
+    this._popup.removeEventListener('click', this._handleButtonClose);
+  }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 import Section from './Section.js';
+import Popup from './Popup.js';
 
 import {
   popups,
